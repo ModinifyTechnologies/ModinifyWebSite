af0cbd103a0d86a2c6dcd00eb0dc764d78453cbd
rferraresi <richard.ferraresi@modinify.com>
2014-06-04 11:40:14 +0530

    New configs again....

--- ModinifyWebSite/src/java/test/com/modinify/test/MyRunTestClass.java
+++ ModinifyWebSite/src/java/test/com/modinify/test/MyRunTestClass.java
@@ -1,9 +1,14 @@
 package com.modinify.test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class MyRunTestClass {
 
+	@Test
 	public void myFirstTest(){
-		
+		assertTrue(1 == 0);
 	}
 
 }
