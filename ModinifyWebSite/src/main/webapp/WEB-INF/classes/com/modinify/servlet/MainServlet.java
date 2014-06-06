9cb822f0ede0588dad25da03853b523a65cc04ee
rferraresi <richard.ferraresi@modinify.com>
2014-06-06 11:04:39 +0530

    Few conf changes - this version is not working properly...

---
+++ ModinifyWebSite/src/main/webapp/WEB-INF/classes/com/modinify/servlet/MainServlet.java
@@ -0,0 +1,42 @@
+package com.modinify.servlet;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+   
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Locale locale = request.getLocale();
+		// This is an example how to set manually locale
+		// locale = new Locale("pt", "BR");
+		ResourceBundle bundle = ResourceBundle.getBundle("com.modinify.messages.index", locale);
+		
+		// only for tests proposes...
+		response.getWriter().println("Hello!");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
